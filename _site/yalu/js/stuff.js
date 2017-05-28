d3395b975595f91461cc4a49bfe7570c02e0ec9f
Luca Cambiaghi <lc.cambiaghi@gmail.com>
2017-05-28 22:05:13 +0200

    updated

--- _site/yalu/js/stuff.js
+++
@@ -1,6 +0,0 @@
-$('#download').click(function() {
-    $(this).text('Downloading...');
-    setTimeout(function() {
-        window.location.replace("itms-services://?action=download-manifest&url=https://lccambiaghi.github.io/yalu/manifest.plist");
-    }, 800);
-});
