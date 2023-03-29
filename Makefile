dd2705edaaa58fd2e0134335f72f130ba445a9f5
Luca Cambiaghi <luca.cambiaghi@me.com>
2023-03-29 12:01:13 +0200

    Adapted to new setup

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .DEFAULT_GOAL := build
-EMACS := /Applications/Emacs.app/Contents/MacOS/Emacs
-SITES_DIR := /Users/luca/Sites
+EMACS := /opt/homebrew/bin/emacs
+SITES_DIR := /Users/cambiaghiluca/Sites
+SITES_DIR := /Library/WebServer/Documents
 
 clean:
 	rm -rf $(SITES_DIR)/*
@@ -9,3 +10,7 @@ build: clean
 	$(EMACS) --script publish.el
 	cp -r output/* $(SITES_DIR)/
 	open -a Safari http://localhost/index.html
+
+setup:
+	sudo chmod -R o+w $(SITES_DIR)
+	mkdir -p $(SITES_DIR)
--- publish.el
+++ publish.el
@@ -1,110 +1,139 @@
 ;;; publish.el --- Generate a simple static HTML blog
 (defun init-use-package ()
-	(require 'package)
-	(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-	(package-initialize)
-	(unless (package-installed-p 'use-package)
-		(package-refresh-contents)
-		(package-install 'use-package)))
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-initialize)
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package)))
 
 (defun setup-deps-github ()
-	(init-use-package)
-	;; Install and configure dependencies
-	(use-package templatel :ensure t)
-	(use-package htmlize :ensure t)
-	(setq org-html-htmlize-output-type 'css)
-	(use-package weblorg :ensure t)
-	;; code blocks syntax highlight
-	(use-package clojure-mode :ensure t)
-	(use-package nix-mode :ensure t)
-	;; set github URL
-	(setq weblorg-default-url "https://lucacambiaghi.com")
-	)
+  (init-use-package)
+  ;; Install and configure dependencies
+  (use-package dash :ensure t)
+  (use-package templatel :ensure t)
+  (use-package htmlize :ensure t)
+  (setq org-html-htmlize-output-type 'css)
+  (use-package weblorg :ensure t)
+  ;; code blocks syntax highlight
+  (use-package clojure-mode :ensure t)
+  (use-package nix-mode :ensure t)
+  ;; set github URL
+  (setq weblorg-default-url "https://lucacambiaghi.com")
+  )
 
 (defun suppress-indentation-message ()
-	(require 'cl-lib)
-	(advice-add 'sh-set-shell :around
-							(lambda (orig-fun &rest args)
-								(cl-letf (((symbol-function 'message) #'ignore))
-									(apply orig-fun args)))))
+  (require 'cl-lib)
+  (advice-add 'sh-set-shell :around
+              (lambda (orig-fun &rest args)
+                (cl-letf (((symbol-function 'message) #'ignore))
+                  (apply orig-fun args)))))
+
+(defun return-subdir-path (subdir-substring dir)
+  "Return full path to subdir"
+  (let ((subdir (car (directory-files dir t subdir-substring))))
+    (if subdir
+        (expand-file-name subdir)
+      nil)))
+
+(setq elpa-base-dir "/Users/cambiaghiluca/.config/crafted-emacs/elpa/")
 
 (defun setup-deps-local ()
-	(load (concat (getenv "HOME") "/.emacs.d/straight/repos/templatel/templatel.el"))
-	(load (concat (getenv "HOME") "/.emacs.d/straight/repos/emacs-htmlize/htmlize.el"))
-	(load (concat (getenv "HOME") "/.emacs.d/straight/repos/weblorg/weblorg.el"))
-	(load (concat (getenv "HOME") "/.emacs.d/straight/repos/clojure-mode/clojure-mode.el"))
-	(add-to-list 'load-path (concat (getenv "HOME") "/.emacs.d/straight/repos/nix-mode"))
-	(load (concat (getenv "HOME") "/.emacs.d/straight/repos/nix-mode/nix-mode.el"))
-	(setq org-html-htmlize-output-type 'css)
-	(setq weblorg-default-url "http://localhost") 	;; "http://localhost:8000"
-	(suppress-indentation-message)
-	)
+  (require 'subr-x)
+  (thread-first
+    "templatel" (return-subdir-path elpa-base-dir) (concat "/templatel.el") (load))
+
+  (thread-first
+    "emacs-htmlize" (return-subdir-path elpa-base-dir) (concat "/htmlize.el" ) (load))
+  (setq org-html-htmlize-output-type 'css)
+
+  (thread-first
+    "weblorg" (return-subdir-path elpa-base-dir) (concat "/weblorg.el" ) (load))
+
+  (thread-first
+    "clojure-mode" (return-subdir-path elpa-base-dir) (concat "/clojure-mode.el" ) (load))
+
+  (thread-first
+    "dash" (return-subdir-path elpa-base-dir) (concat "/dash.el" ) (load))
+
+  (thread-first
+    "magit-section" (return-subdir-path elpa-base-dir) (concat "/magit-section.el" ) (load))
+
+  (add-to-list 'load-path (return-subdir-path "nix-mode" elpa-base-dir))
+  (thread-first
+    "nix-mode" (return-subdir-path elpa-base-dir) (concat "/nix-mode.el" ) (load))
+
+  (setq weblorg-default-url "http://localhost")     ;; "http://localhost:8000"
+  (suppress-indentation-message)
+  )
 
 (defun setup-site ()
-	(if (string= (getenv "ENV") "GHUB")
-			(setup-deps-github)
-		(setup-deps-local))
+  (if (string= (getenv "ENV") "GHUB")
+      (setup-deps-github)
+    (setup-deps-local))
 
-	(setq site-template-vars '(("project_github" . "https://github.com/lccambiaghi/lccambiaghi")
-														 ;; ("static_path" . "$site/static")
-														 ("site_author" . "Luca Cambiaghi")
-														 ("site_name" . "Luca's blog")
-														 ("site_url" . "https://lucacambiaghi.com")
-														 ("site_email" . "luca.cambiaghi@me.com")
-														 ("project_description" . "Luca's blog")))
+  (setq site-template-vars '(("project_github" . "https://github.com/lccambiaghi/lccambiaghi")
+                             ;; ("static_path" . "$site/static")
+                             ("site_author" . "Luca Cambiaghi")
+                             ("site_name" . "Luca's blog")
+                             ("site_url" . "https://lucacambiaghi.com")
+                             ("site_email" . "luca.cambiaghi@me.com")
+                             ("project_description" . "Luca's blog")))
 
-	(weblorg-site
-	 :base-url weblorg-default-url
-	 :template-vars site-template-vars)
+  (weblorg-site
+   :base-url weblorg-default-url
+   :template-vars site-template-vars)
 
-	(weblorg-route
-	 :name "posts"
-	 :input-pattern "posts/*.org"
-	 :template "post.html"
-	 :output "output/posts/{{ slug }}.html"
-	 :url "/posts/{{ slug }}.html")
+  (weblorg-route
+   :name "posts"
+   :input-pattern "posts/*.org"
+   :template "post.html"
+   :output "output/posts/{{ slug }}.html"
+   :url "/posts/{{ slug }}.html")
 
-	;; route for rendering the index page of the blog
-	(weblorg-route
-	 :name "index"
-	 :input-pattern "posts/*.org"
-	 :input-aggregate #'weblorg-input-aggregate-all-desc
-	 :template "blog.html"
-	 :output "output/index.html"
-	 :url "/")
+  ;; route for rendering the index page of the blog
+  (weblorg-route
+   :name "index"
+   :input-pattern "posts/*.org"
+   :input-aggregate #'weblorg-input-aggregate-all-desc
+   :template "blog.html"
+   :output "output/index.html"
+   :url "/")
 
-	;; route for rendering each page
-	(weblorg-route
-	 :name "pages"
-	 :input-pattern "pages/*.org"
-	 :template "page.html"
-	 :output "output/{{ slug }}.html"
-	 :url "/{{ slug }}.html")
+  ;; route for rendering each page
+  (weblorg-route
+   :name "pages"
+   :input-pattern "pages/*.org"
+   :template "page.html"
+   :output "output/{{ slug }}.html"
+   :url "/{{ slug }}.html")
 
-	;; (weblorg-route
-	;;  :name "personal"
-	;;  :input-pattern "pages/personal/*.org"
-	;;  :template "page.html"
-	;;  :output "output/{{ slug }}.html"
-	;;  :url "/{{ slug }}.html")
+  ;; (weblorg-route
+  ;;  :name "personal"
+  ;;  :input-pattern "pages/personal/*.org"
+  ;;  :template "page.html"
+  ;;  :output "output/{{ slug }}.html"
+  ;;  :url "/{{ slug }}.html")
 
-	(weblorg-route
-	 :name "feed"
-	 :input-pattern "posts/*.org"
-	 :input-aggregate #'weblorg-input-aggregate-all-desc
-	 :template "feed.xml"
-	 :output "output/feed.xml"
-	 :url "/feed.xml"
-	 :template-vars site-template-vars
-	 )
+  (weblorg-route
+   :name "feed"
+   :input-pattern "posts/*.org"
+   :input-aggregate #'weblorg-input-aggregate-all-desc
+   :template "feed.xml"
+   :output "output/feed.xml"
+   :url "/feed.xml"
+   :template-vars site-template-vars
+   )
 
-	(weblorg-copy-static
-	 :output "output/static/{{ file }}"
-	 :url "/static/{{ file }}")
-	)
+  (weblorg-copy-static
+   :output "output/static/{{ file }}"
+   :url "/static/{{ file }}")
+  )
 
 ;; (setq debug-on-error t)
 (setup-site)
+
+(require 'advice)
 (weblorg-export)
 
 ;;; publish.el ends here
